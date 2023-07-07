bd487b84ebb4a028935500fd0dce6a013ac7245a
leronbergelson <133677161+CFIALeronB@users.noreply.github.com>
2023-07-07 14:16:55 -0400

    Fixing enviornment variables issues
    - Fixing environment variable REACT_APP_BACKEND_URL
    - Modifying vite.config.ts to define REACT_APP_BACKEND_URL as an environment variable.

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
     plugins: [react()],
     build: {
-        outDir: "../../dist",
-        emptyOutDir: true,
-        sourcemap: true
+      outDir: '../../dist',
+      emptyOutDir: true,
+      sourcemap: true,
     },
-
     server: {
-        proxy: {
-            "/ask": "http://localhost:5000",
-            "/chat": "http://localhost:5000"
-        }
-    }
+      proxy: {
+        '/ask': 'http://localhost:5000',
+        '/chat': 'http://localhost:5000',
+      },
+    },
+    define: {
+      'process.env.REACT_APP_BACKEND_URL': JSON.stringify(
+        env.REACT_APP_BACKEND_URL
+      )
+    },
+  };
 });
